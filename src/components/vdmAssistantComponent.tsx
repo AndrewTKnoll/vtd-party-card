ee5cb3c87e843ce327cada4cfd6feb7cad9b0fab
Andrew Knoll <andrewtknoll@gmail.com>
2022-01-20 20:32:00 -0600

    Added VDM Assistant component.

---
+++ src/components/vdmAssistantComponent.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+interface VDMAssistantComponentProps {}
+interface VDMAssistantComponentState {}
+
+export class VDMAssistantComponent extends Component<VDMAssistantComponentProps, VDMAssistantComponentState> {
+
+	override render() {
+		return (<></>);
+	}
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,13 @@
 /**** style import ****/
 import "./main.scss";
+
+/**** js imports ****/
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { VDMAssistantComponent } from "components/vdmAssistantComponent";
+
+/**** initialize editor ****/
+
+const wrapper = document.querySelector(".vdm-assistant");
+ReactDOM.render(<VDMAssistantComponent/>, wrapper);
