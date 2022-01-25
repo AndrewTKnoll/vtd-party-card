4dd1e1a3795120702ea2a421ce6705249b9345ac
Andrew Knoll <andrewtknoll@gmail.com>
2022-01-25 00:23:46 -0600

    Added modal component.

---
+++ src/components/structure/modalComponent.tsx
@@ -0,0 +1,78 @@
+import React, { Component, MouseEvent } from "react";
+
+import { HeaderLevel } from "base/utilities";
+
+interface ModalComponentProps {
+	openButtonText: string;
+	closeButtonText?: string;
+	title: string;
+	titleHeaderLevel: HeaderLevel;
+	contentClass?: string;
+}
+
+interface ModalComponentState {
+	shown: boolean
+}
+
+export class ModalComponent extends Component<ModalComponentProps, ModalComponentState> {
+
+	constructor(props: ModalComponentProps) {
+		super(props);
+
+		this.state = {
+			shown: false
+		};
+	}
+
+	private toggleShown(shown: boolean) {
+		this.setState({
+			shown: shown
+		});
+		document.body.style.overflow = shown ? "hidden" : "";
+	}
+
+	private frameClicked(event: MouseEvent<HTMLElement>) {
+		if (!(event.target as HTMLElement).closest(".modal-component__window")) {
+			this.toggleShown(false);
+		}
+	}
+
+	override render() {
+		const HeaderType = this.props.titleHeaderLevel;
+
+		const contentWrapperClasses = ["modal-component__content"];
+		if (this.props.contentClass) {
+			contentWrapperClasses.push(this.props.contentClass);
+		}
+
+		return (
+			<section className={`modal-component${this.state.shown ? " modal-component--shown" : ""}`}>
+				<button type="button"
+					className="modal-component__open-button"
+					onClick={this.toggleShown.bind(this, true)}>
+
+					{this.props.openButtonText}
+				</button>
+				<div className="modal-component__frame"
+					onClick={this.frameClicked.bind(this)}>
+					<div className="modal-component__window">
+						<div className="modal-component__header">
+							<HeaderType className="modal-component__title">
+								{this.props.title}
+							</HeaderType>
+							<button type="button"
+								className="modal-component__close-button"
+								onClick={this.toggleShown.bind(this, false)}>
+
+								{this.props.closeButtonText || "Close"}
+							</button>
+						</div>
+						<div className={contentWrapperClasses.join(" ")}>
+							{this.props.children}
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
+}
