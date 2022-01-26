96e4805ffa069d78f6d1ddcdfb2b86f736e9f08f
Andrew Knoll <andrewtknoll@gmail.com>
2022-01-26 00:05:09 -0600

    Added collapse component.

---
+++ src/components/structure/collapseComponent.tsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+import { HeaderLevel } from "base/utilities";
+
+interface CollapseComponentProps {
+	headerText: string;
+	headerLevel: HeaderLevel;
+	contentClass?: string;
+}
+
+interface CollapseComponentState {
+	collapsed: boolean;
+}
+
+export class CollapseComponent extends Component<CollapseComponentProps, CollapseComponentState> {
+
+	constructor(props: CollapseComponentProps) {
+		super(props);
+
+		this.state = {
+			collapsed: true
+		};
+	}
+
+	private toggleCollapse() {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+
+	override render() {
+		const HeaderType = this.props.headerLevel;
+
+		const sectionClasses = ["collapse-component"];
+		if (this.state.collapsed) {
+			sectionClasses.push("collapse-component--collapsed");
+		}
+
+		const contentClasses = ["collapse-component__body"];
+		if (this.props.contentClass) {
+			contentClasses.push(this.props.contentClass.trim());
+		}
+
+		return (
+			<section className={sectionClasses.join(" ")}>
+				<HeaderType className="collapse-component__header">
+					<button type="button"
+						className="collapse-component__toggle"
+						onClick={this.toggleCollapse.bind(this)}>
+
+						{this.props.headerText}
+					</button>
+				</HeaderType>
+				<div className={contentClasses.join(" ")}>
+					{this.props.children}
+				</div>
+			</section>
+		);
+	}
+}
