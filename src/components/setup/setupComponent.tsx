a7de6a821ff6b4fbac31ccaa06488d4a5f9ce497
Andrew Knoll <andrewtknoll@gmail.com>
2022-02-09 21:22:33 -0600

    Added layout for setup component.

--- src/components/setup/setupComponent.tsx
+++ src/components/setup/setupComponent.tsx
@@ -72,81 +72,88 @@ export class SetupComponent extends Component<SetupComponentProps, SetupComponen
 		return (
 			<CollapseComponent headerText="Setup"
 				headerLevel="h2"
-				contentClass="row">
+				contentClass="setup row">
 
-				<div className="col">
-					<button type="button"
-						onClick={this.fullReset.bind(this)}>
+				<div className="setup__main-col col">
+					<div className="row">
+						<div className="setup__settings-col col">
+							<section className="setup__box">
+								<h3>Settings</h3>
+								<div className="setup__settings-row">
+									<button type="button"
+										onClick={this.fullReset.bind(this)}>
 
-						Full Reset
-					</button>
+										Full Reset
+									</button>
+									<label>
+										Difficulty:
+
+										<ItemListSelectComponent isOptional={false}
+											items={allDifficulties}
+											labelForItem={nameForDifficulty}
+											selectedItem={this.props.data.difficulty}
+											onChange={this.setDifficulty.bind(this)}/>
+									</label>
+								</div>
+								<div className="setup__settings-row">
+									<span>
+										{`Event Password: ${this.props.data.dungeon.eventPasswords[this.props.data.difficulty]}`}
+									</span>
+									<button type="button"
+										onClick={this.copyPasswordToClipboard.bind(this)}>
+
+										Copy to Clipboard
+									</button>
+								</div>
+								<div className="setup__settings-row">
+									<span>
+										Skill Test Links
+									</span>
+									<button type="button"
+										onClick={this.copySkillTestsToClipboard.bind(this)}>
+
+										Copy to Clipboard
+									</button>
+								</div>
+							</section>
+						</div>
+						<div className="setup__time-col col">
+							<section className="setup__box">
+								<h3>Event Times</h3>
+								<label>Ticket Start Time:</label>
+								<input type="datetime-local"
+									value={dateString}
+									onChange={this.startTimeChanged.bind(this)}/>
+								{recapVideoStartTime !== undefined &&
+									<TimerComponent targetDate={recapVideoStartTime}
+										prefixText="Time until recap video:"
+										afterTimeText={"after start"}/>
+								}
+								{introVideoStartTime !== undefined &&
+									<TimerComponent targetDate={introVideoStartTime}
+										prefixText="Time until intro video:"
+										afterTimeText={"after start"}/>
+								}
+								{room1StartTime !== undefined &&
+									<TimerComponent targetDate={room1StartTime}
+										prefixText="Time until room one:"
+										afterTimeText={"after start"}/>
+								}
+							</section>
+						</div>
+					</div>
 				</div>
-				<label className="col">
-					Difficulty:
-
-					<ItemListSelectComponent isOptional={false}
-						items={allDifficulties}
-						labelForItem={nameForDifficulty}
-						selectedItem={this.props.data.difficulty}
-						onChange={this.setDifficulty.bind(this)}/>
-				</label>
-				<div className="col">
-					<span>
-						{`Event Password: ${this.props.data.dungeon.eventPasswords[this.props.data.difficulty]}`}
-					</span>
-					<button type="button"
-						onClick={this.copyPasswordToClipboard.bind(this)}>
-
-						Copy to Clipboard
-					</button>
-				</div>
-				<div className="col">
-					<span>
-						Skill Test Links
-					</span>
-					<button type="button"
-						onClick={this.copySkillTestsToClipboard.bind(this)}>
-
-						Copy to Clipboard
-					</button>
-				</div>
-				<div className="col">
-					<h3>
-						Event Times
-					</h3>
-					<label>
-						Ticket Start Time:
-						<input type="datetime-local"
-							value={dateString}
-							onChange={this.startTimeChanged.bind(this)}/>
-					</label>
-					{recapVideoStartTime !== undefined &&
-						<TimerComponent targetDate={recapVideoStartTime}
-							prefixText="Time until recap video:"
-							afterTimeText={"after start"}/>
-					}
-					{introVideoStartTime !== undefined &&
-						<TimerComponent targetDate={introVideoStartTime}
-							prefixText="Time until intro video:"
-							afterTimeText={"after start"}/>
-					}
-					{room1StartTime !== undefined &&
-						<TimerComponent targetDate={room1StartTime}
-							prefixText="Time until room one:"
-							afterTimeText={"after start"}/>
-					}
-				</div>
-				<div className="col">
-					<h3>
-						Pre-Event Checklist
-					</h3>
-					<ul>
-						{this.props.data.preEventChecklistItems.map((item, index) => {
-							return (
-								<li key={index}>{item}</li>
-							);
-						})}
-					</ul>
+				<div className="setup__checklist-col col">
+					<section className="setup__box">
+						<h3>Pre-Event Checklist</h3>
+						<ul>
+							{this.props.data.preEventChecklistItems.map((item, index) => {
+								return (
+									<li key={index}>{item}</li>
+								);
+							})}
+						</ul>
+					</section>
 				</div>
 			</CollapseComponent>
 		);
