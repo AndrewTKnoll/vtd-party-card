69abe68edb2782a15c0ee48c3fe6b6af375da2b0
Andrew Knoll <andrewtknoll@gmail.com>
2022-04-08 22:48:41 -0500

    Added survivor and casualty lists to epilogue component.

--- src/components/epilogue/epilogueComponent.tsx
+++ src/components/epilogue/epilogueComponent.tsx
@@ -2,6 +2,10 @@ import React, { Component, ReactNode } from "react";
 
 import { DataManager } from "model/dataManager";
 import { nameForDifficulty } from "model/attributes/difficulty";
+import { nameForClass } from "model/partyCard/class";
+import { Player } from "model/partyCard/player";
+
+import "utilities/arrayUtils";
 
 interface EpilogueComponentProps {
 	data: DataManager;
@@ -10,7 +14,25 @@ interface EpilogueComponentState {}
 
 export class EpilogueComponent extends Component<EpilogueComponentProps, EpilogueComponentState> {
 
+	private renderPlayerList(players: Player[]): ReactNode {
+		return <ul className="epilogue-component__player-list">
+			{players.map((player) => {
+				return (
+					<li key={player.class}
+						className="epilogue-component__player-label">
+
+						{nameForClass(player.class)}
+					</li>
+				);
+			})}
+		</ul>;
+	}
+
 	override render(): ReactNode {
+		const [deadPlayers, livePlayers] = this.props.data.partyCard.presentPlayers.separate((player) => {
+			return player.isDead;
+		});
+
 		return <div className="epilogue-component">
 			<h2 className="epilogue-component__title">
 				Epilogue
@@ -22,6 +44,14 @@ export class EpilogueComponent extends Component<EpilogueComponentProps, Epilogu
 						<span>Difficulty:</span>
 						<span>{nameForDifficulty(this.props.data.difficulty)}</span>
 					</div>
+				</div>
+				<div className="epilogue-component__survivors-col col">
+					<h3>Survivors</h3>
+					{this.renderPlayerList(livePlayers)}
+				</div>
+				<div className="epilogue-component__casualties-col col">
+					<h3>Casualties</h3>
+					{this.renderPlayerList(deadPlayers)}
 				</div>
 			</div>
 		</div>;
