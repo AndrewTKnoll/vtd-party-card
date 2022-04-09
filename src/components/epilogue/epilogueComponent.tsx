adfcd76df3145968ab91cd2e6a5bf7d12cb9597b
Andrew Knoll <andrewtknoll@gmail.com>
2022-04-08 22:32:41 -0500

    Added difficulty view to epilogue component.

--- src/components/epilogue/epilogueComponent.tsx
+++ src/components/epilogue/epilogueComponent.tsx
@@ -1,6 +1,7 @@
 import React, { Component, ReactNode } from "react";
 
 import { DataManager } from "model/dataManager";
+import { nameForDifficulty } from "model/attributes/difficulty";
 
 interface EpilogueComponentProps {
 	data: DataManager;
@@ -14,6 +15,15 @@ export class EpilogueComponent extends Component<EpilogueComponentProps, Epilogu
 			<h2 className="epilogue-component__title">
 				Epilogue
 			</h2>
+			<div className="row">
+				<div className="epilogue-component__info-col col">
+					<h3>info</h3>
+					<div className="epilogue-component__info-line">
+						<span>Difficulty:</span>
+						<span>{nameForDifficulty(this.props.data.difficulty)}</span>
+					</div>
+				</div>
+			</div>
 		</div>;
 	}
 }
