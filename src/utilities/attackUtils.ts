848ad54538d68d1ddb7db5599249c3ae2edac4e5
Andrew Knoll <andrewtknoll@gmail.com>
2022-04-13 22:47:45 -0500

    Added check taunt utility function.

---
+++ src/utilities/attackUtils.ts
@@ -0,0 +1,19 @@
+import { Monster } from "model/dungeon/monster";
+import { Class } from "model/partyCard/class";
+import { PartyCard } from "model/partyCard/partyCard";
+import { Player } from "model/partyCard/player";
+
+export function checkTaunt<T>(monster: Monster, party: PartyCard, generator: (dwarf: Player[]) => T): T {
+	const dwarf = party.player(Class.dwarf);
+	const guarded = dwarf.isGuarded;
+
+	if (monster.isTaunted) {
+		dwarf.isGuarded = false;
+	}
+
+	const result = generator(monster.isTaunted ? [dwarf] : []);
+
+	dwarf.isGuarded = guarded;
+
+	return result;
+}
