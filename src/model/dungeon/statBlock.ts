3c30c9434ab43e6a3895ead139c84c174425233a
Andrew Knoll <andrewtknoll@gmail.com>
2022-05-13 21:50:10 -0500

    Added numeric value to stat block item model.

--- src/model/dungeon/statBlock.ts
+++ src/model/dungeon/statBlock.ts
@@ -5,7 +5,8 @@ import { DefaultMap } from "utilities/defaultMap";
 
 interface StatBlockItemData {
 	readonly label: string;
-	readonly value: string | { [key: string]: string };
+	readonly numericValue?: number | { [key: string]: number } | undefined;
+	readonly value?: string | ((number: number) => string) | { [key: string]: string } | undefined;
 }
 
 export class StatBlock {
@@ -26,24 +27,33 @@ class StatBlockItem {
 
 	readonly label: string;
 
-	private _value: string | DefaultMap<Difficulty, string>;
-	get value(): string {
-		if (typeof(this._value) === "string") {
-			return this._value;
+	private _numericValue: number | DefaultMap<Difficulty, number> | undefined;
+	get numericValue(): number {
+		if (typeof(this._numericValue) === "object") {
+			return this._numericValue.get(this.dataManager.difficulty);
 		}
-		return this._value.get(this.dataManager.difficulty);
+		return this._numericValue ?? 0;
+	}
+
+	private _value: string | ((number: number) => string) | DefaultMap<Difficulty, string> | undefined;
+	get value(): string {
+		if (typeof(this._value) === "object") {
+			return this._value.get(this.dataManager.difficulty);
+		}
+		if (typeof(this._value) === "function") {
+			return this._value(this.numericValue);
+		}
+		return this._value ?? `${this.numericValue}`;
 	}
 
 	constructor(dataManager: DataManager, data: StatBlockItemData) {
 		this.dataManager = dataManager;
 
 		this.label = data.label;
+		this.id = data.id;
 
-		if (typeof(data.value) === "string") {
-			this._value = data.value;
-		}
-		else {
-			this._value = new DefaultMap("", data.value);
-		}
+		this._numericValue = (typeof(data.numericValue) === "object") ? new DefaultMap(0, data.numericValue) : data.numericValue;
+
+		this._value = (typeof(data.value) === "object") ? new DefaultMap("", data.value) : data.value;
 	}
 }
